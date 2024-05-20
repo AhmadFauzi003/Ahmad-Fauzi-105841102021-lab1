0127a0cdf7fe0e37420846ecd248d5b55e7dfe8f
AhmadFauzi003 <105841102021@student.unismuh.ac.id>
2024-05-20 14:42:29 +0800

    mantap

--- App.js
+++ App.js
@@ -1,22 +1,57 @@
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={{flex: 1,}}>
-      <View style = {{flex: 1,flexDirection: 'row'}}>
-       <View style={{flex : 1, flexDirection: 'column'}}>
-       <View style={{flex : 1, backgroundColor: 'black'}}></View>
-       <View style={{flex : 1, backgroundColor: 'grey'}}></View>
-     </View>
-     <View style={{flex : 1,flexDirection: 'column'}}>
-      <View style={{flex : 1,flexDirection: 'row'}}>
-      <View style={{flex : 1, backgroundColor: 'yellow'}}></View>
-      <View style={{flex : 1, backgroundColor: 'red'}}></View>
-       </View>
-      </View>
+    <SafeAreaView style = {{
+      flex: 1,
+      backgroundColor: 'white',
+    }}>
+  <View style={{ 
+    flex : 1, 
+    backgroundColor : 'white',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    marginBottom : 100
+    }}>
+
+    <View style={{ 
+     backgroundColor: 'red',
+     width : 150,
+     height : 80,
+     borderRadius : 10,
+     marginRight : 10,
+     }}> 
+       <Text style={{
+        color : 'white',
+        textAlign : 'center',
+        lineHeight : 80,
+        fontSize : 30,
+        fontWeight : 'bold',
+      }}>
+        Login
+      </Text>
     </View>
-    <View style={{flex : 1, backgroundColor : 'green'}}></View>
+    <View style={{ 
+     backgroundColor: 'blue',
+     width : 150,
+     height : 80,
+     borderRadius : 10,
+     marginRight : 10,
+     }}> 
+       <Text style={{
+        color : 'white',
+        textAlign : 'center',
+        lineHeight : 80,
+        fontSize : 30,
+        fontWeight : 'bold',
+      }}>
+        Register
+      </Text>
     </View>
+  </View>
+  </SafeAreaView>
   );
 }
+
